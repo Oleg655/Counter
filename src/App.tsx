64dec21ca254e70368551266f3a839b97c6596bf
Oleg655 <savinoleg95@gmail.com>
2021-03-09 17:38:18 +0300

    first commit counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const main = {
+        width: '300px',
+        height: '200px',
+        padding: '20px',
+        border: '2px solid blue',
+        margin: '150px auto'
+    }
+
+    const counter = {
+        width: '280px',
+        height: '100px',
+        border: '2px solid blue'
+    }
+
+    const inc = {
+        width: '102px',
+        height: '50px',
+        marginBottom: '70px'
+    }
+
+    const reset = {
+        width: '182px',
+        height: '50px',
+        marginTop: '20px'
+    }
+    let [value, setValue] = useState(0)
+    return (
+
+        <div style={main}>
+            <div style={counter} >{value}</div>
+            <div>
+                <button style={inc} disabled={false} onClick={() => {
+                    setValue(value++)
+                    if(value === 5){
+                        
+                    }
+                }}>inc
+                </button>
+                <button style={reset}  disabled={true}  onClick={() => {setValue(0)}}>reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
